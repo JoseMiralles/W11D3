08ae64d3fa12bea58231f108550418455865d628
Jose Miralles <josemiralles1991@gmail.com>
2021-01-13 14:43:34 -0500

    React is now connected and rendering in the 'root' div.

---
+++ frontend/entry.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const root = document.getElementById("root");
+
+    ReactDOM.render(
+        <h1>Hi</h1>,
+        root
+    );
+
+});
